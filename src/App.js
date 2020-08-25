48bf561890f89cd9edf701ed91c66523c3201bc7
Serzh108 <serzh@gabris.kiev.ua>
2020-08-25 23:26:41 +0300

    Initial v1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import RegistrationPage from './pages/auth/RegistrationPage';
-import LoginPage from './pages/auth/LoginPage';
 import logo from './assets/images/logo.png';
 import './App.css';
 
@@ -10,8 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>GoIT Bootcamp19 Final project</p>
-        <RegistrationPage />
-        {/* <LoginPage /> */}
       </header>
     </div>
   );
