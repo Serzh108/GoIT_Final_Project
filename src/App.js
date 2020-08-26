f3e04a0d9a60fdeb99972c046fa3b9cce14d58ea
irynailina <irynailina@gmail.com>
2020-08-26 13:57:39 +0300

    merged dev in ira branch without conflicts

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import logo from './assets/images/logo.png';
 import './App.css';
 import SideBarHabits from './components/SideBarHabits';
+import RegistrationPage from './pages/auth/RegistrationPage';
 
-function App() {
+const App = () => {
   return (
-    // <SideBarHabits />
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>GoIT Bootcamp19 Final project</p>
-        <RegistrationPage />
-        {/* <LoginPage /> */}
-      </header>
-    </div>
+    <SideBarHabits />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>GoIT Bootcamp19 Final project</p>
+    //     <RegistrationPage />
+    //     {/* <LoginPage /> */}
+    //   </header>
+    // </div>
   );
-}
+};
 
 export default App;
