943de063a403674e980c88fcd741d745ac0e46c3
irynailina <irynailina@gmail.com>
2020-08-27 23:28:40 +0300

    dataStructure added as a text document

--- src/pages/dashboard/StatisticsPage.js
+++ src/pages/dashboard/StatisticsPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './statisticsPage.module.css';
 import Header from '../../components/Header/Header';
 import Progress from '../../components/Progress/Progress';
+
 function StatisticsPage() {
   return (
     <>
