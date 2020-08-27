147c572f10792c540632c2d69f63be0af3ce5a46
Ghore <ghoreua@gmail.com>
2020-08-27 16:18:28 +0300

    save code before megre

---
+++ src/pages/dashboard/StatisticsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './statisticsPage.module.css';
+import Header from '../../components/Header/Header';
+import Progress from '../../components/Progress/Progress';
+function StatisticsPage() {
+  return (
+    <>
+      <Header />
+
+      <div className={style.sideBar}>
+        <div className={style.date}></div>
+      </div>
+      <Progress />
+    </>
+  );
+}
+
+export default StatisticsPage;
