77906005034626eadb179f250a2b30607f232ca0
NatTuardy <nattuardy@gmail.com>
2020-08-27 21:21:14 +0300

    fixed

--- src/components/DateTable/DateTable.js
+++ src/components/DateTable/DateTable.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import ReactHorizontalDatePicker from 'react-horizontal-strip-datepicker';
-import 'react-horizontal-strip-datepicker/dist/ReactHorizontalDatePicker.css';
 import css from './DateTable.module.css';
 
 const DateTable = ({ backData }) => {
-  const onSelectedDay = d => {
-    return Date(Date.now());
-    console.log(d);
-  };
-
   const getCalendarDay = i => {
     const now = new Date(new Date().setDate(new Date().getDate() - i));
     const weekDay = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
     const day = weekDay[now.getDay()];
-    console.log('now.getDay()', now.getDay());
-    console.log('day', day);
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const data = now.getDate();
     const currentDate = `${data}.${month}`;
-    console.log('currentDate', currentDate);
 
     return (
       <td className={css.box}>
