f3b29bef9663787731374f21c333e7a593a80a07
irynailina <irynailina@gmail.com>
2020-08-29 01:56:21 +0300

    styles fixed, button logic done, console mistakes cleaned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './assets/images/logo.png';
 import './App.css';
-import SideBarHabits from './components/SideBarHabits/SideBarHabits';
 import RegistrationPage from './pages/auth/RegistrationPage';
-import ExitModal from './components/ExitModal/ExitModal';
-import DeleteHabitModal from './components/DeleteHabitModal/DeleteHabitModal';
-import Table from './components/Table/Table';
-import DateTable from './components/DateTable/DateTable';
-import MainTable from './components/MainTable/MainTable';
+import LoginPage from './pages/auth/LoginPage';
 import StatisticsPage from './pages/dashboard/StatisticsPage';
+
 const App = () => {
   return (
     <>
       <StatisticsPage />
-      {/* <DeleteHabitModal/>
- <ExitModal/> */}
-      {/* <MainTable /> */}
-      {/* <Table/> */}
-      {/* <SideBarHabits /> */}
-
       {/* <RegistrationPage /> */}
       {/* <LoginPage /> */}
     </>
